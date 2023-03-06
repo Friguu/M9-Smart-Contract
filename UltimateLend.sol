0dbebe79526c688105a0e3dcd84787f6f0837db3
Friguu <benedict.conrady@gmail.com>
2023-03-06 17:49:12 +0100

    comments added

--- UltimateLend.sol
+++ UltimateLend.sol
@@ -3,7 +3,19 @@ pragma solidity ^0.8
 contract UltimateLend {
 
     constructor() {
-        
+
     }
+
+    //+borrow_money
+    //+lend_money
+    //+get_open_debts
+    //+request_lend_money
+    //+pay_debts
+    //+calc_fees
+
+    //modifiers:
+    //+debts_payed
+    //+is_lender
+    //+has_custom_token
 
 }
